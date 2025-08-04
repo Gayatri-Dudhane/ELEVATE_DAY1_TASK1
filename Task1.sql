d2cd1125fffd8622370ce095c2dabe22089aa78b
Gayatri-Dudhane <gayatridudhane9958@gmail.com>
2025-08-04 21:11:48 +0530

    DAY1_TAsk

---
+++ Task1.sql
@@ -0,0 +1,7 @@
+copy property from 'D:\DATA ANALYTICS\SQL\PropertyData.csv' DELIMITER ',' csv header
+
+select * from property
+
+select * from property where type = 'land'
+
+select * from property where qty between 1 and 5
